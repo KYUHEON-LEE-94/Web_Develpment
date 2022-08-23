41bce2de4b0c72b34f38068ec792b09950dc4f81
maizurzu <101496219+maizurzu@users.noreply.github.com>
2022-08-23 17:51:38 +0900

    Add files via upload
    부트스트랩 끝, JS 기초 시작

---
+++ FrontEnd-Workspace/namoo.js/JS/hello.js
@@ -0,0 +1,18 @@
+
+let message = '안녕하세요..'
+console.log(message)
+let h2 = document.getElementsByTagName('h2');
+let a = document.createElement('a');
+a.style.display = 'block'
+h2[0].appendChild(a).innerHTML
+console.log(childa);
+
+function sayHello(message){
+    alert(message)
+    h2[0].innerText = "누르지 말았어야했어.."
+    a.style.backgroundColor = 'red';
+    h2[0].appendChild(a).innerHTML = "짜잔!"
+}
+
+$('h2').mouse
+
