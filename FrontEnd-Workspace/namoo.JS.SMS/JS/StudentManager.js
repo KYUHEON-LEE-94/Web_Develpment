4416a563fa7ce07070b09c65cc6b283ff028af99
maizurzu <101496219+maizurzu@users.noreply.github.com>
2022-09-03 01:51:54 +0900

    Add files via upload
    전체 검색 기능 및 약간의 필터링 기능 추가

--- FrontEnd-Workspace/namoo.JS.SMS/JS/StudentManager.js
+++ FrontEnd-Workspace/namoo.JS.SMS/JS/StudentManager.js
@@ -1,45 +1,53 @@
 
 
 //Array를 이용한 학생 성정 관리
-class StudentManager{
+class StudentManager {
 
     //static 프로퍼티
     static schoolname = "Korea IT School"
 
 
 
-    constructor(){
+    constructor() {
         //this ={}가 존재하는 상황
-        this.students= [];
+        this.students = [];
     }
 
     //배열에 학생 등록 기능
-    addStudent(student){
+    addStudent(student) {
         this.students.push(student);
     }
 
     //배열에 저장된 정체 학생 목록 반환
-    listAll(){
+    listAll() {
         return this.students
     }
 
     //학생이름으로 찾아오기
-    findnames(name){
+    findnames(name) {
         const findnames = this.students.find(student => student.name === name)
-        return findnames
-        
+        if (findnames === undefined) {
+            return 0
+        } else {
+            return findnames
+        }
+
     }
 
     //학번으로 찾기
-    findssn(ssn){
+    findssn(ssn) {
         const findssn = this.students.find(student => student.ssn === parseInt(ssn))
-        return findssn
-        
+        if (findssn === undefined) {
+            return 0
+        } else {
+            return findssn
+        }
+
     }
 
-    toString(){
+    toString() {
         return `${this.ssn}  ${this.name} ${this.korean} ${this.eng} ${this.math}`
     }
 }
 
-export{StudentManager}
+export { StudentManager }
--- FrontEnd-Workspace/namoo.JS.SMS/JS/app.js
+++ FrontEnd-Workspace/namoo.JS.SMS/JS/app.js
@@ -1,14 +1,14 @@
 // 애플리케이션 엔트리 포인트
-import {Student} from "./Student.js"
-import {StudentManager} from "./StudentManager.js"
+import { Student } from "./Student.js"
+import { StudentManager } from "./StudentManager.js"
 
 
 //테스트 객체화
-let cat = new Student(1,"고양이", 30, 20, 10);
+let cat = new Student(1, "고양이", 30, 20, 10);
 let studentManager = new StudentManager();
 studentManager.addStudent(cat);
-studentManager.addStudent(new Student(2,"이규헌",10,20,30));
-studentManager.addStudent(new Student(3,"강아지",20,30,40));
+studentManager.addStudent(new Student(2, "이규헌", 10, 20, 30));
+studentManager.addStudent(new Student(3, "강아지", 20, 30, 40));
 
 //---객체와 여기까지--
 //변수 선언---------------------
@@ -19,11 +19,6 @@ let status = document.getElementById('status');
 
 let trchildren = document.getElementsByClassName('tr');
 
-
-function test (){
-    console.log(trchildren[1].children[0]);
-}
-test()
 //input 분류------------
 let ssninput = document.getElementById('ssninput');
 let studentnameinput = document.getElementById('studentnameinput');
@@ -52,10 +47,13 @@ let ssnBtnSearch = document.getElementById('ssnBtnSearch');
 //등록 버튼
 let enrollBtn = document.getElementById('enroll');
 
+//전체 검색 버튼
+let SearchAll = document.getElementById('SearchAll');
+
 //변수 선언 여기까지..
 
 //학생 정보들 출력 함수
-function regitinfo(){
+function regitinfo() {
     studentnamevalue = studentnameinput.value;
     ssninputValue = ssninput.value;
     korInputValue = korInput.value;
@@ -65,61 +63,89 @@ function regitinfo(){
 }
 //HTML 테이블 화면에 학생의 정보들을 입력해주는 함수
 //student 객체를 인자로 받아서 해당 인자의 ssn,name등의 정보를 각 테이블 위치에 입력해서 화면으로 보여줌
-function tableshow(findobj){
-    stussn.textContent = findobj.ssn
-    stuname.textContent = findobj.name;
-    stukor.textContent = findobj.kor;
-    stueng.textContent = findobj.eng;
-    stumath.textContent = findobj.math;
+function tableshow(findobj) {
+    if (findobj === 0) {
+        status.textContent = "유효하지 않는 값입니다."
+    } else {
+        stussn.textContent = findobj.ssn;
+        stuname.textContent = findobj.name;
+        stukor.textContent = findobj.kor;
+        stueng.textContent = findobj.eng;
+        stumath.textContent = findobj.math;
+
+        status.textContent = '';
+    }
+}
+
+function searchAll() {
+    inputAllvalue()
+    status.textContent = '';
+
+}
+//각 테이블의 tr의 자식 값들을 찾아서 각위치에 ssn과 name등의 값을 넣어서 HTML에 표시해줌
+function inputAllvalue() {
+    let list = studentManager.listAll()
+    for (let i = 0; i < trchildren.length; i++) {
+        for (let j = 1; j < trchildren[i].children.length; j++) {
+
+            switch (j) {
+                case 1: trchildren[i].children[j].textContent = list[i].ssn; break;
+                case 2: trchildren[i].children[j].textContent = list[i].name; break;
+                case 3: trchildren[i].children[j].textContent = list[i].kor; break;
+                case 4: trchildren[i].children[j].textContent = list[i].eng; break;
+                case 5: trchildren[i].children[j].textContent = list[i].math; break;
+            }
+        }
+
+    }
 }
 
 
 //h2 타이틀 적어주기
-h2.innerText = Student.schoolname +" 학생 성적 관리"
+h2.innerText = Student.schoolname + " 학생 성적 관리"
 
 //-----학생명 검색버튼으로 성적 정보 찾기
-function searchName(){
+function searchName() {
     regitinfo()
     let findname = studentManager.findnames(studentnamevalue);
-
     tableshow(findname);
 }
-studentnameinput.onkeyup = enterkey;
-stNameBtn.onclick = searchName; 
+stNameBtn.onclick = searchName;
 
 //-----학번 검색버튼으로 성적 정보 찾기
-function searchSSN(){
+function searchSSN() {
     regitinfo()
     let findssn = studentManager.findssn(ssninputValue);
     tableshow(findssn);
-    enterkey()
 }
-function enterkey(e) {
-	if (e.keyCode == 13) {
-        searchSSN()
-    }
-}
-ssninput.onkeyup =enterkey
-ssnBtnSearch.onclick =searchSSN;
+
+ssnBtnSearch.onclick = searchSSN;
 
 //----StudentManager에 student객체를 생성해서 등록해주는 함수
-function enrollment(){
+function enrollment() {
     regitinfo();
-    let student = new Student(ssninputValue, studentnamevalue,korInputValue, engInputValue, mathInputValue)
-    
-    studentManager.addStudent(student);
-    status.textContent = studentnamevalue + " 학생의 성적 정보 추가 완료"
+
+    if (ssninputValue === '') {
+        status.textContent = "학번을 입력해 주세요"
+        if (studentnamevalue === '') {
+            status.textContent = "이름을 입력해 주세요"
+        }
+    } else {
+        let student = new Student(ssninputValue, studentnamevalue, korInputValue, engInputValue, mathInputValue)
+        studentManager.addStudent(student);
+        status.textContent = studentnamevalue + " 학생의 성적 정보 추가 완료"
+    }
 }
 //등록 버튼을 누르면 해당 정보가 등록됨
 enrollBtn.onclick = enrollment;
 
 
 //----전체검색
-function Allsearch(){
-    let some = studentManager.listAll()
-    console.dir(some);
+function Allsearch() {
+    searchAll();
+
 }
-Allsearch();
+SearchAll.onclick = Allsearch;
 
 
 
