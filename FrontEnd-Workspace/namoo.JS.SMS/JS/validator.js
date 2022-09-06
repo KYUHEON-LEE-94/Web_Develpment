a0ebf6425657390aeb459f6dbf85fb92221e1c8c
maizurzu <101496219+maizurzu@users.noreply.github.com>
2022-09-07 01:12:54 +0900

    Add files via upload
    유효성 검사 기능 추가

--- FrontEnd-Workspace/namoo.JS.SMS/JS/validator.js
+++ FrontEnd-Workspace/namoo.JS.SMS/JS/validator.js
@@ -2,18 +2,20 @@
 let Validator = {};
 
 /** 입력 여부 검증 */
-Validator.isNull = function(value){
-  return value != undefined || value.trim().length == 0;
+Validator.isAvailable = function (value) {
+  let regex = /^[a-zA-Z가-힣]+/gmi;
+  return value != undefined && (value.trim().length > 0 && regex.test(value));
 }
 
 /** 전화번호 형식 검증 */
-Validator.isTelephone = function(telephone){
+Validator.isTelephone = function (telephone) {
   let regex = /\d{2,3}[- .]\d{3,4}[- .]\d{4}/gm;
   return regex.test(telephone);
 }
 /**학번 형식 검색 */
-Validator.isSSN = function(ssn){
-  let regex = /\d{2,}-+\d{1,}/gm;
-  return regex.test(ssn);
+Validator.isNumber = function (...ssn) {
+  let regex = /\d{1,}/gm;
+  let Check = regex.test(ssn)
+  return Check
 }
-export {Validator};
+export { Validator };
